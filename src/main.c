95d1954a3de170ca5dcfaa6bb99ea50e3637a5a8
rcarvs <renan.carvalho.sousa@gmail.com>
2017-11-16 11:25:47 -0200

    Criada a tabela e verificando bit presente/ausente

--- src/main.c
+++ src/main.c
@@ -1,46 +1,83 @@
 #include <stdio.h>
 #include <stdlib.h>
-
-
-
-
+#include "tabela.c"
 
 int main (int argc, char *argv[]){
     
-    FILE *fp = fopen("/home/juaumpc/Dev/SO/TP2/Simulador-de-Memoria-Virtual/Arquivos Complementares/Entradas_TP2_2017/compilador.log","r");
+    if(argc != 5){
+        printf("Formato de entrada inválido.\n");
+        return 1;
+    }
+
+    /*
+    --------------------------------------------------------
+    |           LEITURA DOS PARAMETROS INICIAIS            |
+    --------------------------------------------------------
+    */
+    char *algoritmo_substituicao = argv[1];
+    char *arquivo_entrada = argv[2];
+    int tamanho_pagina = atoi(argv[3]);
+    int tamanho_memoria = atoi(argv[4]);
     
-    if(fp == NULL){
-        printf("erro na abertura do arquivo");
+    /*
+    Abertura e controle dos parametros de entrada
+    */
+    FILE *arquivo = fopen(arquivo_entrada,"r");
+
+    if(arquivo == NULL){
+        printf("Arquivo de entrada não pode ser aberto\n");
+        return 1;
     }
 
+    
+    //[TODO]CRIAR AQUI VERIFICAÇÃO SE O ALGORITIMO DE SUBSTITUIÇÃO É CORRETO
+    //[TODO] VERIFICAR TAMANHO PAGINA E TAMANHO DA MEMORIA
 
-    while(!feof(fp)){
-        //jardelcarvalho
-        unsigned addr;
-        char rw;
-        fscanf(fp,"%x %c", &addr,&rw);
-        printf("%x %c\n",addr,rw);
 
-        switch(&argv[1]){
+
+    /*
+    Criar a tabela de páginas de acordo com o tamanho especificado.
+    */    
+    tabela *tabela;
+
+    criaTabela(tabela,tamanho_pagina);
+
+    
+    /*
+    leitura dos acessos.
+    */
+    int tmpPOS;
+    char tmpOP;
+
+    int indice;
+
+
+    int pageFaut = 0;
+    
+
+    while(fscanf(arquivo,"%x %c\n",&tmpPOS,&tmpOP) != EOF){
+        indice = tmpPOS%tabela->num_entradas;
+        //achei a pagina, agora vou acessar o conteudo dela
+        if(tabela->paginas[indice].presente){
+            //pagina esta na memoria principal e e seu endereço é a moldura com o deslocamento
             
-            case "lru":
-            //Algoritmo lru
-            break;
+        }else{
+            //pagina nao esta na memoria principal
+            pageFaut++;
+            //inventar uma pagina e substituir usando o algoritmo de substituicao
+            printf("%s\n",algoritmo_substituicao );
+            if(algoritmo_substituicao == "fifo" || algoritmo_substituicao == "FIFO"){
+                printf("FIFO\n");
+            }
 
-            case "fifo":
-            //Alg fifo
-            break;
-
-            case "random":
-            //Alg random
-            break;
         }
-
-
+        //printf("%d\n", indice);
+        //break;
     }
 
 
 
 
+    
     return 0;
 }
